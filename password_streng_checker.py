a134801cdd6f458bd78e70ed4e5e98179ca0cacf
Kbalazs3 <kbalazs93@gmail.com>
2023-08-03 14:01:21 +0200

    password input funciton created, main function started

---
+++ password_streng_checker.py
@@ -0,0 +1,10 @@
+# This is a practicing project to create a password streng checker.
+
+
+def ask_password():
+    asked_password = str(input("Enter the password:"))
+    return asked_password
+
+
+def main():
+# This will be the main function.
